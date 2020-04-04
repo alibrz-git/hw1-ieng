01989de19c383477e68cb92a478cf82d1ddd9ee4
ali-brz <48012032+ali-brz@users.noreply.github.com>
2020-04-04 16:55:52 +0430

    Logger and Polygon Validation added

--- index.js
+++ index.js
@@ -3,27 +3,25 @@ const app = express()
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const isPointInsidePolygon = require('point-in-polygon')
+var GJV = require("geojson-validation");
+var log = require('debug-logger')('index');
+
+
 
 app.use(bodyParser.json())
 
 let polygons = {}
 
-fs.readFile(`${__dirname}/data.json`,function(err,data){
-    if(err) throw err;
-    polygons  = JSON.parse(data.toString())
+fs.readFile(`${__dirname}/data.json`, function (err, data) {
+    if (err) throw err;
+    polygons = JSON.parse(data.toString())
 })
 
-
-app.put('/gis/addpolygon',function(req,res,next){
-    polygons.features.push(req.body)
-    fs.writeFile(`${__dirname}/data.json`,JSON.stringify(polygons),(err)=>{
-       if(err) res.sendStatus(500) 
-	res.sendStatus(200)
-    })
-    
-})
-app.get('/gis/testpoint',function(req,res,next){
+app.get('/gis/testpoint', function (req, res, next) {
     let result = { polygons: [] };
+    if(!req.query.lat || !req.query.long){
+        res.status(400).send("lat and long are required")
+    }
     polygons.features.forEach(element => {
         if (isPointInsidePolygon([req.query.long, req.query.lat], element.geometry.coordinates[0])) {
             result.polygons.push(element.properties.name)
@@ -32,4 +30,22 @@ app.get('/gis/testpoint',function(req,res,next){
     res.send(result)
 })
 
-app.listen(process.env.PORT || 8000,() => console.log(`Example app listening on port ${process.env.PORT || 8000}!`))
+app.put('/gis/addpolygon', function (req, res, next) {
+    log.debug(`put request to /gis/polygon with req body : ${req.body}`)
+    GJV.isFeature(req.body, function (valid, errs) {
+        if (!valid) {
+            log.debug(`${errs} \nreq body : ${req.body}`)
+            res.status(500).send(errs)
+        } else {
+            polygons.features.push(req.body)
+            fs.writeFile(`${__dirname}/data.json`, JSON.stringify(polygons), (err) => {
+                if (err) res.sendStatus(500)
+                log.debug(`polygon saved successfully : ${req.body}`)
+                res.sendStatus(200)
+            })
+        }
+    });
+})
+
+
+app.listen(process.env.PORT || 8000, () => console.log(`Example app listening on port ${process.env.PORT || 8000}!`))
